daa62387b8442ae87f7b58847573494f663846ff
ajeet <ajeet214.90@gmail.com>
2023-06-07 09:53:39 +0700

    added a file

--- lidl_GB.py
+++ lidl_GB.py
@@ -27,3 +27,16 @@ wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "button.nuc-m-butt
 wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'input[placeholder="Enter post code or city"]'))).send_keys('London')
 
 time.sleep(10)
+
+
+"""
+Few things to note:
+
+1. First, as we hit the URL, a cookie pops up that we can accept to continue. So we wait for this pop-up and click on the ACCEPT button.
+2. Next, we wait for the STORE SEARCH button to get located and then click.
+3. It loads a side search box where we can enter the city or the postcode to search. So we wait for this to get loaded/located in order to enter the query. we can use send_keys() method to enter/input either the city name or the postcode.
+
+for example, as we enter the city name/postcode (London), a dropdown list appears with available stores in that region, you can choose accordingly and proceed further.
+reference:
+https://stackoverflow.com/questions/76392044/how-can-i-locate-and-enter-text-in-the-search-box-on-lidls-website-using-seleni
+"""
