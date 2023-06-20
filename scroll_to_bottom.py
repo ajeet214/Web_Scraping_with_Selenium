321e52fe4b4b92a15da2938958d3584ecb1be27b
ajeet <ajeet214.90@gmail.com>
2023-06-20 10:09:43 +0700

    added a file

--- scroll_to_bottom.py
+++ scroll_to_bottom.py
@@ -32,3 +32,17 @@ while flag:
     else:
         last_height = new_height
 
+"""
+steps followed:
+
+1. First, we wait for the scroll-bar web element to get visibly located/loaded on the page and assign it to a variable 
+scroll_bar
+2. Next, we get the current height of this scroll_bar and assign it to a variable last_height.
+3. start looping and in each iteration, scroll down to the bottom of the scroll bar, take a pause, get the height of 
+the scroll bar, and assign it to a variable new_height and check if the new_height == last_height, break out of the 
+loop(flag=Flase) otherwise, update the variable last_height with new_height and repeat this step until the if condition
+ is True.
+ 
+reference:
+https://stackoverflow.com/questions/76503251/how-to-scroll-down-to-the-bottom-of-an-inner-scroll-bar-using-selenium-with-pyth
+"""
